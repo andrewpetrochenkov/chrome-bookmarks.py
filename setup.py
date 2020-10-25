5d664160c77808678992adef6bf9057d3dd066d8
andrewp-as-is <russianidiot.github@gmail.com>
2020-10-25 10:45:17 +0300

    2020:10:25 10:45

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='chrome-bookmarks',
+    version='2020.10.25',
     install_requires=open('requirements.txt').read().splitlines(),
     packages=setuptools.find_packages()
 )
